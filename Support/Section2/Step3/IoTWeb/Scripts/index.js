cec51830921166980a243b552a836be5de39a4cd
ms-iotkithol-jp <ms-iotkithol-jp@outlook.jp>
2017-11-15 17:38:02 +0900

    Add Pressure Graph

--- Support/Section2/Step3/IoTWeb/Scripts/index.js
+++ Support/Section2/Step3/IoTWeb/Scripts/index.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     var timeData = [],
-        tempData = [];
-    var data = {
+        tempData = [],
+        presData = [];
+    var dataTempG = {
         labels: timeData,
         datasets: [
             {
@@ -18,10 +19,27 @@ $(document).ready(function () {
         ]
     }
 
-    var basicOption = {
+    var dataPresG = {
+        labels: timeData,
+        datasets: [
+            {
+                fill: false,
+                label: 'Environment',
+                yAxisID: 'Pressure',
+                borderColor: "rgba(2, 204, 0, 1)",
+                pointBoarderColor: "rgba(2, 204, 0, 1)",
+                backgroundColor: "rgba(2, 204, 0, 0.4)",
+                pointHoverBackgroundColor: "rgba(2, 204, 0, 1)",
+                pointHoverBorderColor: "rgba(2, 204, 0, 1)",
+                data: presData
+            }
+        ]
+    }
+
+    var basicOptionTemp = {
         title: {
             display: true,
-            text: 'Envrionment Real-time Data',
+            text: 'Envrionment Real-time Temperature Data',
             fontSize: 36
         },
         scales: {
@@ -37,29 +55,57 @@ $(document).ready(function () {
         }
     }
 
+    var basicOptionPress = {
+        title: {
+            display: true,
+            text: 'Envrionment Real-time Pressure Data',
+            fontSize: 36
+        },
+        scales: {
+            yAxes: [{
+                id: 'Pressure',
+                type: 'linear',
+                scaleLabel: {
+                    labelString: 'Pressure',
+                    display: true
+                },
+                position: 'left',
+            }]
+        }
+    }
+
     //Get the context of the canvas element we want to select
-    var ctx = document.getElementById("myChart").getContext("2d");
+    var ctxTemp = document.getElementById("tempChart").getContext("2d");
     var optionsNoAnimation = { animation: false }
-    var myLineChart = new Chart(ctx, {
+    var tempLineChart = new Chart(ctxTemp, {
         type: 'line',
-        data: data,
-        options: basicOption
+        data: dataTempG,
+        options: basicOptionTemp
+    });
+    var ctxPress = document.getElementById("pressChart").getContext("2d");
+    var presLineChart = new Chart(ctxPress, {
+        type: 'line',
+        data: dataPresG,
+        options: basicOptionPress
     });
     var hub = $.connection.envHub;
     hub.on("Environment", function (packet) {
-        if (!packet.time || !packet.temperature) {
+        if (!packet.time || !packet.temperature || !packet.pressure) {
             return;
         }
         timeData.push(packet.time);
         tempData.push(packet.temperature);
+        presData.push(packet.pressure);
 
         // only keep no more than 50 points in the line chart
         var len = timeData.length;
-        if (len > 50) {
+        if (len > 200) {
             timeData.shift();
             tempData.shift();
+            presData.shift();
         }
-        myLineChart.update();
+        tempLineChart.update();
+        presLineChart.update();
     });
     $.connection.hub.start();
 });
